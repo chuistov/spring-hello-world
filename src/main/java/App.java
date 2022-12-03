4583a06e698a307813a6c6be5dd0cd4dd776b44a
chuistov <108229048+chuistov@users.noreply.github.com>
2022-12-03 22:30:43 +0300

    Cat class added AppConfig class updated - getCat method added

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,12 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        var bean1 = applicationContext.getBean("helloworld", HelloWorld.class);
+        System.out.println(bean == bean1);
+
+        var cat = applicationContext.getBean("cat", Cat.class);
+        var cat1 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(cat == cat1);
+
     }
 }
